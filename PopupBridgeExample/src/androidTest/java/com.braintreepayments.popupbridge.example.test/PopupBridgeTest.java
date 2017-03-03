2f82328a80dfbded99ebbcd170c37b2396207592
lkorth <luke@lukekorth.com>
2017-03-03 12:09:39 -0500

    Wait for page to load in browser to prevent test failures

--- PopupBridgeExample/src/androidTest/java/com.braintreepayments.popupbridge.example.test/PopupBridgeTest.java
+++ PopupBridgeExample/src/androidTest/java/com.braintreepayments.popupbridge.example.test/PopupBridgeTest.java
@@ -13,6 +13,8 @@ import static com.lukekorth.deviceautomator.UiObjectMatcher.withContentDescripti
 @RunWith(AndroidJUnit4.class)
 public class PopupBridgeTest {
 
+    private static final long BROWSER_TIMEOUT = 20000;
+
     @Before
     public void setup() {
         onDevice().onHomeScreen().launchApp("com.braintreepayments.popupbridge.example");
@@ -36,21 +38,23 @@ public class PopupBridgeTest {
     @Test(timeout = 30000)
     public void opensPopup_whenClickingDontLikeAnyOfTheseColors_returnsCanceledSelection() {
         onDevice(withContentDescription("Launch Popup")).perform(click());
-        onDevice(withContentDescription("I don't like any of these colors")).perform(click());
+        onDevice(withContentDescription("I don't like any of these colors"))
+                .waitForExists(BROWSER_TIMEOUT).perform(click());
         onDevice(withContentDescription("You do not like any of these colors")).waitForExists();
     }
 
     @Test(timeout = 30000)
     public void opensPopup_whenClickingBack_returnsCanceledSelection() {
         onDevice(withContentDescription("Launch Popup")).perform(click());
-        onDevice(withContentDescription("I don't like any of these colors")).waitForExists();
+        onDevice(withContentDescription("I don't like any of these colors"))
+                .waitForExists(BROWSER_TIMEOUT);
         onDevice().pressBack();
         onDevice(withContentDescription("You did not choose a color")).waitForExists();
     }
 
     private void testColor(String color) {
         onDevice(withContentDescription("Launch Popup")).perform(click());
-        onDevice(withContentDescription(color)).perform(click());
+        onDevice(withContentDescription(color)).waitForExists(BROWSER_TIMEOUT).perform(click());
         onDevice(withContentDescription("Your favorite color:")).waitForExists();
         onDevice(withContentDescription(color.toLowerCase())).waitForExists();
     }
