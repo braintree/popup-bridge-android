89fa84cbf46f093bc0adb0c61ea1d74fd6b8abea
Sarah Koop <skoop@paypal.com>
2020-09-21 09:30:04 -0500

    Update integration tests to assert that text exists (#34)
    * Update integration tests to check text contains string
    
    * Update tests to follow browser-switch assertion format
    
    * Update travis.yml script to match browser-switch

--- PopupBridgeExample/src/androidTest/java/com.braintreepayments.popupbridge.example.test/PopupBridgeTest.java
+++ PopupBridgeExample/src/androidTest/java/com.braintreepayments.popupbridge.example.test/PopupBridgeTest.java
@@ -2,7 +2,7 @@ package com.braintreepayments.popupbridge.example.test;
 
 import android.os.Build;
 
-import androidx.test.runner.AndroidJUnit4;
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
 
 import com.lukekorth.deviceautomator.DeviceAutomator;
 
@@ -14,8 +14,9 @@ import static com.lukekorth.deviceautomator.AutomatorAction.click;
 import static com.lukekorth.deviceautomator.DeviceAutomator.onDevice;
 import static com.lukekorth.deviceautomator.UiObjectMatcher.withContentDescription;
 import static com.lukekorth.deviceautomator.UiObjectMatcher.withText;
+import static org.junit.Assert.assertTrue;
 
-@RunWith(AndroidJUnit4.class)
+@RunWith(AndroidJUnit4ClassRunner.class)
 public class PopupBridgeTest {
 
     private static final long BROWSER_TIMEOUT = 20000;
@@ -46,7 +47,7 @@ public class PopupBridgeTest {
         onViewWithText("Launch Popup").perform(click());
         onViewWithText("I don't like any of these colors")
                 .waitForExists(BROWSER_TIMEOUT).perform(click());
-        onViewWithText("You do not like any of these colors").waitForExists();
+        assertTrue(onDevice(withText("You did not like any of our colors")).exists());
     }
 
     @Test(timeout = 50000)
@@ -55,14 +56,14 @@ public class PopupBridgeTest {
         onViewWithText("I don't like any of these colors")
                 .waitForExists(BROWSER_TIMEOUT);
         onDevice().pressBack();
-        onViewWithText("You did not choose a color").waitForExists();
+        assertTrue(onDevice(withText("You did not choose a color")).exists());
     }
 
     private void testColor(String color) {
         onViewWithText("Launch Popup").perform(click());
         onViewWithText(color).waitForExists(BROWSER_TIMEOUT).perform(click());
         onViewWithText("Your favorite color:").waitForExists();
-        onViewWithText(color.toLowerCase()).waitForExists();
+        assertTrue(onDevice(withText(color.toLowerCase())).exists());
     }
 
     private DeviceAutomator onViewWithText(String text) {
