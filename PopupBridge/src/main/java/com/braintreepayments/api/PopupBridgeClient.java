3e0e547e60c1c91fe6557b1aa277d93ae3fb18fc
sshropshire <sshropshire@paypal.com>
2022-12-14 13:07:07 -0600

    Revert TODO to next major version note.

--- PopupBridge/src/main/java/com/braintreepayments/api/PopupBridgeClient.java
+++ PopupBridge/src/main/java/com/braintreepayments/api/PopupBridgeClient.java
@@ -17,7 +17,7 @@ import java.util.Set;
 
 public class PopupBridgeClient {
 
-    // TODO: consider using a `com.braintreepayments...` prefixed request key
+    // NEXT MAJOR VERSION: consider using a `com.braintreepayments...` prefixed request key
     // to prevent shared preferences collisions with other braintree libs that use browser switch
     static final int REQUEST_CODE = 1;
 
