ae3d9bfbee315137a97c33ae9f6ff7a1789f16a4
Timothy Chow <tichow@paypal.com>
2025-04-24 12:28:24 -0500

    Remove wildcard import

--- PopupBridge/src/test/java/com/braintreepayments/api/internal/PopupBridgeJavascriptInterfaceUnitTest.kt
+++ PopupBridge/src/test/java/com/braintreepayments/api/internal/PopupBridgeJavascriptInterfaceUnitTest.kt
@@ -1,6 +1,9 @@
 package com.braintreepayments.api.internal
 
-import org.junit.Assert.*
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertNull
+import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
 
